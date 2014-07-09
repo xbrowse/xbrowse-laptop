02653ff7c622fcc02a3b07a89eaedce8a546e12f
Brett Thomas <brettpthomas@gmail.com>
2014-07-09 15:37:07 -0400

    Fixing bug in VEP path

--- xbrowse_settings/annotator_settings.py
+++ xbrowse_settings/annotator_settings.py
@@ -3,7 +3,7 @@ db_port = 27017
 db_name = 'xbrowse_annotator'
 
 vep_perl_path = '/home/vagrant/variant_effect_predictor/variant_effect_predictor.pl'
-vep_cache_dir = '/home/vagrant/.vep/'
+vep_cache_dir = '/home/vagrant'
 vep_batch_size = 50000
 
 reference_populations = [
